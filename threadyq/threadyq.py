d7abe4ab9a25d15db207250a34413094efbbf234
Subburayalu Muthusamy <subburayalu.muthusamy@MT202301-0258.local>
2025-02-24 18:14:50 +0530

    Scheduled based threadyq

--- threadyq/threadyq.py
+++ threadyq/threadyq.py
@@ -1,6 +1,7 @@
 import queue
 import threading
-from typing import Callable, Any
+from typing import Callable, Any, Optional
+from datetime import datetime, timedelta
 
 
 def print_response(prefix: str, msg: str, color: str = "success"):
@@ -14,6 +15,7 @@ def print_response(prefix: str, msg: str, color: str = "success"):
     colors = {
         "success": '\033[92m',  # Green
         "error": '\033[91m',    # Red
+        "scheduled": '\033[93m',    #Yellow
     }
     color_code = colors.get(color)
     end_color = '\033[0m'
@@ -33,28 +35,50 @@ class ThreadyQ:
     def _worker(self):
         """Worker function to process tasks from the queue."""
         while True:
-            task, args = self.task_queue.get()  # Get the next task and its arguments
+            current_time = datetime.now()
+            task_time, task, args = self.task_queue.get() # Get the next task and its arguments
             if task is None:  # Exit signal
                 break
             try:
-                task(*args)  # Execute the task with unpacked arguments
+                if task_time <= current_time:
+                    task(*args) 
+                    # print(f"Executed task {task.__name__} at {current_time}")
+                    print_response(prefix="Executed task", msg = f"{task.__name__} at {task_time}",color="scheduled") 
+                    
+                else:
+                    self.task_queue.put((task_time, task, args))
+                    
             except Exception as e:
-                print_response(prefix="error", msg = f"Error processing task: {e}",color="error")
+                print_response(prefix="error", msg = f"Error processing task: {e}",color="error") 
             finally:
                 self.task_queue.task_done()  # Mark the task as done
 
-
-    def put_the_task(self, task: Callable, *args: Any):
+    def put_the_task(self, task: Callable, schedule_time: Optional[str] = None, *args: Any):
         """
         Add a task to the queue with optional arguments.
         
         :param task: The task function to be executed.
+        :param schedule_time: Optional scheduled time in 'HH:MM' format (24-hour).
         :param args: The arguments to pass to the task.
         """
         if not callable(task):
             raise ValueError("The task must be a callable function or method.")
         
-        self.task_queue.put((task, args))  # Put the task and its args in the queue
+        if schedule_time:
+            hours, minutes = map(int, schedule_time.split(':'))
+
+            now = datetime.now()
+
+            scheduled_time = now.replace(hour=hours, minute=minutes, second=0, microsecond=0)
+
+            if scheduled_time <= now:
+                scheduled_time += timedelta(days=1)
+        
+        else:
+            scheduled_time = datetime.now()
+
+        
+        self.task_queue.put((scheduled_time, task, args))  # Put the task and its args in the queue with scheduled time
         print(f"Task {task.__name__} added. Current task count: {self.get_task_count()}")
 
 
